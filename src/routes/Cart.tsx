eaaf8a9ad4750244e702cbdd0ae000ce8f6ad1ec
wingrove210 <126200528+wingrove210@users.noreply.github.com>
2024-05-09 23:58:58 +0300

    Корзина готова
    Корзина готова

--- src/routes/Cart.tsx
+++ src/routes/Cart.tsx
@@ -11,10 +11,10 @@ import { selectCart } from '../redux/cart/selectors'
 import { HiPlusSm } from "react-icons/hi";
 import { HiMinusSm } from "react-icons/hi";
 import cutlery from '../assets/images/cutlery_2.svg';
-import bus from '../assets/images/cutlery_2.svg'
-import money from '../assets/images/cutlery_2.svg'
-import way_to_pay from '../assets/images/cutlery_2.svg'
-import promo from '../assets/images/cutlery_2.svg'
+import bus from '../assets/images/bus.svg'
+import money from '../assets/images/money_hand.svg'
+import comment from '../assets/images/list_items.svg'
+import promo from '../assets/images/promocode.svg'
 
 export default function Cart() {
   const dispatch = useDispatch()
@@ -93,15 +93,43 @@ export default function Cart() {
                    <button className='border-2 border-stone-800 rounded-full px-[2px] py-[2px]'><HiPlusSm /></button>
                  </div>
               </div>
-              <div className='flex justify-between px-2 py-2 items-center bg-[#F1F1F1] border-b-2 border-stone-800'>
-                 <div className='flex items-center gap-2'>
-                    <img src={cutlery} alt="" />
+              <div className='flex justify-between px-2 py-4 items-center bg-[#F1F1F1] border-b-2 border-stone-800'>
+                 <div className='flex items-center gap-4 ml-2'>
+                    <img src={bus} alt="" />
                     <div className='flex flex-col gap-1'>
-                       <h2 className='font-term text-xl leading-3'>ПРИБОРЫ</h2>
-                       <p className='font-roboto text-[10]'>бесплатно</p>
+                       <h2 className='font-term text-xl leading-3'>САМОВЫВОЗ</h2>
+                       <p className='font-roboto text-[8px] font-bold'>Адрес:  г.Южно-Сахалинск, улица Мира 231/9</p>
                     </div>
                  </div>
-                 div
+                 <Link to='/delivery' className='uppercase text-[#4D4D4D] border-2 rounded-[5px] border-[#4D4D4D] text-[8px] px-4 py-1 font-bold'>изменить</Link>
+              </div>
+              <div className='flex justify-between px-2 py-4 items-center bg-[#F1F1F1] border-b-2 border-stone-800'>
+                 <div className='flex items-center gap-4 ml-2'>
+                    <img src={money} alt="" />
+                    <div className='flex flex-col gap-1'>
+                       <h2 className='font-term text-xl leading-3'>САМОВЫВОЗ</h2>
+                       <p className='font-roboto text-[8px] font-bold text-red'>Выберите способ оплаты </p>
+                    </div>
+                 </div>
+                 <Link to='/delivery' className='uppercase text-[#4D4D4D] border-2 rounded-[5px] border-[#4D4D4D] text-[8px] px-4 py-1 font-bold'>изменить</Link>
+              </div>
+              <div className='flex justify-between px-2 py-4 items-center bg-[#F1F1F1] border-b-2 border-stone-800'>
+                 <div className='flex items-center gap-4 ml-2'>
+                    <img src={comment} alt="" />
+                    <div className='flex flex-col gap-1'>
+                       <h2 className='font-term text-md leading-4'>комментарии к заказу</h2>
+                    </div>
+                 </div>
+                 <Link to='/delivery' className='uppercase text-[#4D4D4D] border-2 rounded-[5px] border-[#4D4D4D] text-[8px] px-4 py-1 font-bold'>Написать</Link>
+              </div>
+              <div className='flex justify-between px-2 py-4 items-center bg-[#F1F1F1] border-b-2 border-stone-800'>
+                 <div className='flex items-center gap-4 ml-2'>
+                    <img src={promo} alt="" />
+                    <div className='flex flex-col gap-1'>
+                      <input type="text" placeholder='промокод' className='w-[40vw] border-2 border-[#4D4D4D] rounded-lg font-term pl-2'/>
+                    </div>
+                 </div>
+                 <Link to='/delivery' className='uppercase text-[#4D4D4D] border-2 rounded-[5px] border-[#4D4D4D] text-[8px] px-4 py-1 font-bold'>применить</Link>
               </div>
               <div className='cart__bottom-buttons'>
                 <Link to={`/`} className='button button--outline button--add go-back-btn'>
