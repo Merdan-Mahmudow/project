df26277e4f32bbe279a81b28578b1a552eb0c0d0
wingrove210 <126200528+wingrove210@users.noreply.github.com>
2024-05-09 22:39:41 +0300

    Сделал корзину
    Сделал корзину, но нужно довести ее до идеала

--- src/components/CartItem/index.tsx
+++ src/components/CartItem/index.tsx
@@ -6,6 +6,10 @@ import RemoveCartSvg from '../../svg/RemoveCartSvg'
 import { useDispatch } from 'react-redux'
 import { addItem, minusItem, removeItem } from '../../redux/cart/slice'
 import { CartItem as CartItemType } from '../../redux/cart/types'
+import { HiPlusSm } from "react-icons/hi";
+import { HiMinusSm } from "react-icons/hi";
+import { HiTrash } from "react-icons/hi2";
+import { RxCross2 } from "react-icons/rx";
 
 type CartItemProps = {
   id: string,
@@ -46,43 +50,46 @@ export const CartItem: React.FC<CartItemProps> = ({
   }
 
   return (
-    <div className='cart__item'>
+    <div className='flex justify-between bg-[#F1F1F1] border-b-2 border-stone-700 py-2 px-2 gap-2'>
 
-      <div className='cart__item-img'>
+      <div className='flex justify-center items-center'>
         <Link key={id} to={`/pizza/${id}`}>
           <img
-            className='pizza-block__image'
+            className='w-[100px] h-[90px] rounded-[20px]'
             src={image}
             alt='Pizza'
           />
         </Link>
       </div>
-      <div className='cart__item-info'>
-        <Link key={id} to={`/pizza/${id}`}>
-          <h3>{foodName}</h3>
-          <p>{description}</p>
+      <div className='w-[165px]'>
+        <Link key={id} to={`/pizza/${id}`} className='gap-2 flex flex-col'>
+          <h3 className='font-term text-2xl leading-4'>{foodName}</h3>
+          <p className='font-next text-[6px] leading-2'>{description}</p>
+          <div className='flex items-center gap-2'>
+            <button
+              disabled={count === 0}
+              onClick={onClickMinus}
+              className='px-[2px] py-[2px] border-2 border-stone-600 rounded-full text-center'>
+              {/* <MinusCartSvg /> */}
+              <HiPlusSm />
+            </button>
+            <b>{count}</b>
+            <button
+              onClick={onClickPlus}
+              className='px-[2px] py-[2px] border-2 border-stone-600 rounded-full text-center'>
+              {/* <PlusCartSvg /> */}
+              <HiMinusSm />
+            </button>
+          </div>
         </Link>
       </div>
-
-      <div className='cart__item-count'>
-        <button
-          disabled={count === 0}
-          onClick={onClickMinus}
-          className='button button--outline button--circle cart__item-count-minus'>
-          <MinusCartSvg />
-        </button>
-        <b>{count}</b>
-        <button
-          onClick={onClickPlus}
-          className='button button--outline button--circle cart__item-count-plus'>
-          <PlusCartSvg />
-        </button>
-      </div>
-      <div className='cart__item-price'>
-        <b>{price * count} ₽</b>
-      </div>
-      <div onClick={onClickRemove} className='cart__item-remove'>
-        <div className='button button--outline button--circle'><RemoveCartSvg /></div>
+      <div className='flex flex-col w-[90px] self-center items-center gap-1'>
+        <div className='text-right'>
+          <b className='text-xl font-term color w-[80px] text-right text-stone-600'>{price * count} P</b>
+        </div>
+        <div onClick={onClickRemove} className='border-2 border-stone-600 rounded-full px-1 py-1'>
+          <div className=''><RxCross2/></div>
+        </div>
       </div>
     </div>
   )
