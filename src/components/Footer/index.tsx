8f4429b9d26837ec3c9f6e7cd149837da08acde2
wingrove210 <126200528+wingrove210@users.noreply.github.com>
2024-05-10 00:50:35 +0300

    Готов Футер
    Я сделал футер

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import styles from './Footer.module.scss'
+import wabisabi from '../../assets/images/wabisabi.svg'
 
 export const Footer: React.FC = () => {
   return (
-    <footer className='h-10'>
+    <footer className='px-2 pt-3 pb-4 bg-green-600 rounded-b-[40px]'>
+       <div className='flex flex-col items-center gap-1'>
+          <img src={wabisabi} alt="" className=''/>
+          <p className='text-white text-[8px] font-bold text-center'>Сделано в WABISABI. Сервис приема заказов.</p>
+       </div>
     </footer>
   )
 }
