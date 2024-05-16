79ae23ec97d38238eac6c1ad3913d9c26578db7e
Merdan-Mahmudow <softprank@gmail.com>
2024-05-16 10:22:42 +0300

    modified:   src/routes/Cart.tsx 	modified:   src/routes/Comment.tsx 	modified:   tsconfig.json

--- src/routes/Cart.tsx
+++ src/routes/Cart.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect} from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { InfoBox } from '../components/InfoBox'
 import { CartItem } from '../components/CartItem'
@@ -28,27 +28,16 @@ export default function Cart({ initialCount = 1 }) {
       dispatch(clearItems())
     }
   }
-
+  const tg = Telegram.WebApp
+  console.log(tg.initDataUnsafe.chat?.id)
   const onClickPay = () => {
     if (window.confirm(`Вы заказали ${totalCount} пиц на сумму ${totalPrice} ₽`)) {
-      const tg = Telegram.WebApp
-      console.log(tg.initDataUnsafe.chat?.id)
+
       tg.sendData(JSON.stringify(items))
       dispatch(clearItems())
     }
   }
-  const [instCount, setInst] = useState<number>(1)
-  const handleMinus = () => {
-    if(instCount == 0){
-      setInst(0)
-    }
-    else{
-      setInst(instCount - 1)
-    }
-  }
-  const handlePlus = () => {
-    setInst(instCount + 1)
-  }
+
   const [count, setCount] = useState(initialCount);
 
   const handleIncrement = () => {
@@ -67,7 +56,7 @@ export default function Cart({ initialCount = 1 }) {
         <div className='container container--cart'>
           <div className='cart'>
             <div className="flex w-full  bg-red-600 px-2 py-2">
-               <h1 className='text-white font-term text-2xl w-full text-center'>Мой заказ</h1>
+              <h1 className='text-white font-term text-2xl w-full text-center'>Мой заказ</h1>
             </div>
             <div className='content__items'>
               {items.map((item: any) =>
@@ -80,62 +69,59 @@ export default function Cart({ initialCount = 1 }) {
                 <p className='uppercase font-term text-2xl'>{totalPrice} P</p>
               </div>
               <div className='bg-[#F1F1F1] px-2 py-2 border-b-2 border-stone-800'>
-                 <p className='text-[10px] font-roboto font-bold'>Призаказе от 2000р Батат фри с пармезаном всего за 120р Для получения скидки добавьте блюдо в заказ самостоятельно его можно найти в разделе Закуски</p>
+                <p className='text-[10px] font-roboto font-bold'>Призаказе от 2000р Батат фри с пармезаном всего за 120р Для получения скидки добавьте блюдо в заказ самостоятельно его можно найти в разделе Закуски</p>
               </div>
               <div className='flex justify-between px-2 py-2 items-center bg-[#F1F1F1] border-b-2 border-stone-800'>
-                 <div className='flex items-center gap-2'>
-                    <img src={cutlery} alt="" />
-                    <div className='flex flex-col gap-1'>
-                       <h2 className='font-term text-xl leading-3'>ПРИБОРЫ</h2>
-                       <p className='font-roboto text-[10]'>бесплатно</p>
-                    </div>
-                 </div>
-                 <div className='flex gap-2'>
-                   <button className='border-2 border-stone-800 rounded-full px-[2px] py-[2px]' onClick={handleMinus}><HiMinusSm/></button>
-                   <p>{instCount}</p>
-                   <button className='border-2 border-stone-800 rounded-full px-[2px] py-[2px]' onClick={handlePlus}><HiPlusSm /></button>
-                   <button className='border-2 border-stone-800 rounded-full px-[2px] py-[2px]' onClick={handleDecrement}><HiMinusSm/></button>
-                   <p className='font-bold font-roboto'>{count}</p>
-                   <button className='border-2 border-stone-800 rounded-full px-[2px] py-[2px]' onClick={handleIncrement}><HiPlusSm /></button>
-                 </div>
+                <div className='flex items-center gap-2'>
+                  <img src={cutlery} alt="" />
+                  <div className='flex flex-col gap-1'>
+                    <h2 className='font-term text-xl leading-3'>ПРИБОРЫ</h2>
+                    <p className='font-roboto text-[10]'>бесплатно</p>
+                  </div>
+                </div>
+                <div className='flex gap-2'>
+                  <button className='border-2 border-stone-800 rounded-full px-[2px] py-[2px]' onClick={handleDecrement}><HiMinusSm /></button>
+                  <p className='font-bold font-roboto'>{count}</p>
+                  <button className='border-2 border-stone-800 rounded-full px-[2px] py-[2px]' onClick={handleIncrement}><HiPlusSm /></button>
+                </div>
               </div>
               <div className='flex justify-between px-2 py-4 items-center bg-[#F1F1F1] border-b-2 border-stone-800'>
-                 <div className='flex items-center gap-4 ml-2'>
-                    <img src={bus} alt="" />
-                    <div className='flex flex-col gap-1'>
-                       <h2 className='font-term text-xl leading-3'>САМОВЫВОЗ</h2>
-                       <p className='font-roboto text-[8px] font-bold'>Адрес:  г.Южно-Сахалинск, улица Мира 231/9</p>
-                    </div>
-                 </div>
-                 <Link to='/delivery' className='uppercase text-[#4D4D4D] border-2 rounded-[5px] border-[#4D4D4D] text-[8px] px-4 py-1 font-bold'>изменить</Link>
+                <div className='flex items-center gap-4 ml-2'>
+                  <img src={bus} alt="" />
+                  <div className='flex flex-col gap-1'>
+                    <h2 className='font-term text-xl leading-3'>САМОВЫВОЗ</h2>
+                    <p className='font-roboto text-[8px] font-bold'>Адрес:  г.Южно-Сахалинск, улица Мира 231/9</p>
+                  </div>
+                </div>
+                <Link to='/delivery' className='uppercase text-[#4D4D4D] border-2 rounded-[5px] border-[#4D4D4D] text-[8px] px-4 py-1 font-bold'>изменить</Link>
               </div>
               <div className='flex justify-between px-2 py-4 items-center bg-[#F1F1F1] border-b-2 border-stone-800'>
-                 <div className='flex items-center gap-4 ml-2'>
-                    <img src={money} alt="" />
-                    <div className='flex flex-col gap-1'>
-                       <h2 className='font-term text-lg leading-3'>СПОСОБ ОПЛАТЫ</h2>
-                       <p className='font-roboto text-[8px] font-bold text-red'>Выберите способ оплаты </p>
-                    </div>
-                 </div>
-                 <Link to='/payment' className='uppercase text-[#4D4D4D] border-2 rounded-[5px] border-[#4D4D4D] text-[8px] px-4 py-1 font-bold'>изменить</Link>
+                <div className='flex items-center gap-4 ml-2'>
+                  <img src={money} alt="" />
+                  <div className='flex flex-col gap-1'>
+                    <h2 className='font-term text-lg leading-3'>СПОСОБ ОПЛАТЫ</h2>
+                    <p className='font-roboto text-[8px] font-bold text-red'>Выберите способ оплаты </p>
+                  </div>
+                </div>
+                <Link to='/payment' className='uppercase text-[#4D4D4D] border-2 rounded-[5px] border-[#4D4D4D] text-[8px] px-4 py-1 font-bold'>изменить</Link>
               </div>
               <div className='flex justify-between px-2 py-4 items-center bg-[#F1F1F1] border-b-2 border-stone-800'>
-                 <div className='flex items-center gap-4 ml-2'>
-                    <img src={comment} alt="" />
-                    <div className='flex flex-col gap-1'>
-                       <h2 className='font-term text-md leading-4'>комментарии к заказу</h2>
-                    </div>
-                 </div>
-                 <Link to='/comment' className='uppercase text-[#4D4D4D] border-2 rounded-[5px] border-[#4D4D4D] text-[8px] px-4 py-1 font-bold'>Написать</Link>
+                <div className='flex items-center gap-4 ml-2'>
+                  <img src={comment} alt="" />
+                  <div className='flex flex-col gap-1'>
+                    <h2 className='font-term text-md leading-4'>комментарии к заказу</h2>
+                  </div>
+                </div>
+                <Link to='/comment' className='uppercase text-[#4D4D4D] border-2 rounded-[5px] border-[#4D4D4D] text-[8px] px-4 py-1 font-bold'>Написать</Link>
               </div>
               <div className='flex justify-between px-2 py-4 items-center bg-[#F1F1F1] border-b-2 border-stone-800'>
-                 <div className='flex items-center gap-4 ml-2'>
-                    <img src={promo} alt="" />
-                    <div className='flex flex-col gap-1'>
-                      <input type="text" placeholder='промокод' className='w-[40vw] border-2 border-[#4D4D4D] rounded-lg font-term pl-2'/>
-                    </div>
-                 </div>
-                 <Link to='/promo' className='uppercase text-[#4D4D4D] border-2 rounded-[5px] border-[#4D4D4D] text-[8px] px-4 py-1 font-bold'>применить</Link>
+                <div className='flex items-center gap-4 ml-2'>
+                  <img src={promo} alt="" />
+                  <div className='flex flex-col gap-1'>
+                    <input type="text" placeholder='промокод' className='w-[40vw] border-2 border-[#4D4D4D] rounded-lg font-term pl-2' />
+                  </div>
+                </div>
+                <Link to='/promo' className='uppercase text-[#4D4D4D] border-2 rounded-[5px] border-[#4D4D4D] text-[8px] px-4 py-1 font-bold'>применить</Link>
               </div>
               <div className='flex justify-between'>
                 <Link to={`/`} className='font-bold flex justify-between gap-1 items-center px-[10px] py-1 bg-green-500 w-auto'>
--- src/routes/Comment.tsx
+++ src/routes/Comment.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useState } from "react";
 import { Link } from 'react-router-dom'
 import BackArrowSvg from '../svg/BackArrowSvg'
+import comment from '../assets/images/list_items.svg'
 
 interface ButtonProps {
     disabled: boolean;
@@ -16,9 +17,10 @@ const Button: React.FC<ButtonProps> = ({ disabled, style, children }) => {
       </button>
     );
   };
+ 
 export default function Comment(){
-    const [text, setText] = useState('');
-
+    const [text, setText] = useState<string>('');
+    
     const buttonStyle = {
       backgroundColor: text.length === 0 ? 'gray' : 'blue',
       color: 'white',
@@ -29,11 +31,12 @@ export default function Comment(){
     }
     const handleTextChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
         setText(event.target.value)
+        console.log(text)
       }
     return (
        <div className="h-[80vh] bg-[#F1F1F1] pt-3 px-3 flex flex-col gap-2">
             <div className="w-full flex justify-center">
-                <textarea name="text" id="text" placeholder="Ваш комментарий к заказу ..." className="px-2 border-2 border-[#9F9F9F] rounded-[20px] font-roboto font-bold py-2 bg-[#F1F1F1]" value={text} onChange={handleTextChange} rows={5} cols={40}/>
+                <textarea name="text" id="text" placeholder="Ваш комментарий к заказу ..." className="px-2 border-2 border-[#9F9F9F] rounded-[20px] font-roboto font-bold py-2 bg-[#F1F1F1]" onChange={handleTextChange} rows={5} cols={40}/>
             </div>
             <div className="w-full flex justify-end">
                 <button className="bg-stone-400 px-8 py-2 rounded-[8px] text-white font-roboto font-bold text-[12px]" disabled={text.length === 0} style={buttonStyle}>ОТПРАВИТЬ</button>
@@ -43,4 +46,19 @@ export default function Comment(){
             </Link>
        </div>
     )
+}
+
+function CartComment(){
+  return(
+    <div className='flex justify-between px-2 py-4 items-center bg-[#F1F1F1] border-b-2 border-stone-800'>
+             <div className='flex items-center gap-4 ml-2'>
+                <img src={comment} alt="" />
+                <div className='flex flex-col gap-1'>
+                   <h2 className='font-term text-md leading-4'>комментарии к заказу</h2>
+                   <p></p>
+                </div>
+             </div>
+             <Link to='/comment' className='uppercase text-[#4D4D4D] border-2 rounded-[5px] border-[#4D4D4D] text-[8px] px-4 py-1 font-bold'>Написать</Link>
+          </div>
+  )
 }
