8b506a35b890471772db73cd8ac5fc77865085bf
wingrove210 <126200528+wingrove210@users.noreply.github.com>
2024-06-23 21:12:58 +0300

    Текст в карточках
    Текст в карточках

--- src/components/PizzaBlock/index.tsx
+++ src/components/PizzaBlock/index.tsx
@@ -210,12 +210,12 @@ export const PizzaBlock: React.FC<PizzaBlockProps> = ({
         <div className='h-[8vh] mt-1 12pro:h-[7vh] flex flex-col gap-1'>
           <h4 className='text-xl font-term leading-4 tracking-widest'>{foodName}</h4>
           {isTruncated ? (
-            <span className='text-[7px] leading-tight relative'>
+            <span className='text-[7px] leading-tight relative desc-text'>
               {truncatedText}
               {description.length > maxLength * 9 && "..."}
             </span>
           ) : (
-            <span className='text-[7px] leading-tight relative 13mini:text-[7px]'>
+            <span className='text-[7px] leading-tight relative 13mini:text-[7px] desc-text'>
               {description}
             </span>
           )}
