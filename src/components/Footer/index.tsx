ab19d4647b959e7114fefb0fd776342028898ac1
wingrove210 <126200528+wingrove210@users.noreply.github.com>
2024-09-03 01:17:18 +0300

    Поменял ОРГН

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -9,7 +9,7 @@ export const Footer: React.FC = () => {
           <img src={wabisabi} alt="" className='w-[28vw]'/>
           <p className='text-white text-[11.2px] font-bold text-center tracking-widest  font-roboto'>Сделано в WABISABI. Сервис приема заказов.</p>
           <p className='text-white text-[11.2px] font-bold text-center tracking-widest  font-roboto'>ИНН: 650111123528</p>
-          <p className='text-white text-[11.2px] font-bold text-center tracking-widest  font-roboto'>ОГРН: 323650000016972</p>
+          <p className='text-white text-[11.2px] font-bold text-center tracking-widest  font-roboto'>ОГРН: 32265000002216</p>
           <p className='text-white text-[11.2px] font-bold text-center tracking-widest  font-roboto'>Тел: +79147561723</p>
           <p className='text-white text-[11.2px] font-bold text-center tracking-widest  font-roboto'>Эл. почта: m.v.pak@mail.ru</p>
        </div>
